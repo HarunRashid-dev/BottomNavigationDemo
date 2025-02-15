cc00fc6f76fe33b64e84040b233f0765e3f0b9af
Admin <harunrashid9159@gmail.com>
2025-02-15 19:35:55 +0530

    Code Completed till 7:50

--- app/src/main/java/np/com/bimalkafle/bottomnavigationdemo/MainScreen.kt
+++ app/src/main/java/np/com/bimalkafle/bottomnavigationdemo/MainScreen.kt
@@ -13,6 +13,11 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -24,18 +29,26 @@ fun MainScreen(modifier: Modifier = Modifier){
         NavItem("Settings", Icons.Default.Settings),
     )
 
+    var selectedIndex by remember{
+        mutableIntStateOf(0)
+    }
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         bottomBar = {
             NavigationBar {
                 navItemList.forEachIndexed{index, navItem ->
                     NavigationBarItem(
-                        selected = true,
-                        onClick = { },
+                        selected = selectedIndex == index,
+                        onClick = {
+                            selectedIndex = index
+                        },
                         icon = {
                             Icon(imageVector = navItem.icon, contentDescription = "Icon")
                         },
-                        label = {}
+                        label = {
+                            Text(text = navItem.label)
+                        }
                     )
                 }
             }
